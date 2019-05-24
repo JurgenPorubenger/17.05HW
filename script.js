3582e96673e5424f4930818712ef61b888328cd3
Eugene <jurgenporubenger@gmail.com>
2019-05-24 07:51:56 +0300

    21.05HW

--- script.js
+++ script.js
@@ -62,14 +62,88 @@
 //     sum=sum+d;
 //     console.log(sum);
 // }
-let prom = +prompt();
-function f(a,d,progres) {
-    for (i=0;i<prom;i++){
-        progres+=d;
-        console.log(progres);
+// let prom = +prompt();
+// function f(a,d,progres) {
+//     for (i=0;i<prom;i++){
+//         progres+=d;
+//         console.log(progres);
+//     }
+// }
+//
+//
+// f(prom,prom,prom);
 
-    }
-}
+// let arr = [1,2,3,4];
+// console.log(arr[arr.length-1]);
+// let str = "stroka";
+// console.log(str[str.length-1]);
 
 
-f(prom,prom,prom);
+// let him = [];
+// let her = [];
+//
+// let arr = ["gregovna","santavich","cruzovna","gavnovich","sukaich","blyatich"];
+// for (let i=0; i<arr.length; i++) {
+//     let otch=arr[i];
+//     // console.log(otch[otch.length-1]);
+//     if (otch[otch.length-1]=="a"){
+//         her.push(otch);
+//         // console.log(her[]);
+//     }else him.push(otch);
+// }
+// console.log(her);
+// console.log(him);
+
+
+// let arr = [12,23,31,64,5,16];
+// let max=0;
+// let min=0;
+//
+// for(i=0;i<arr.length;i++) {
+//     // console.log(arr[i]);
+//     if (arr[i]>arr[0]){
+//         max=arr[i];
+//         console.log(max);
+//     }
+// }
+let number = [];
+let string = [];
+
+
+let arr = [12,23,31,64,5,"umnica","lavash","zubochistka",[67,"zorro",12],48,125,["zadnica",85, 17] ];
+
+ for(i=0;i<arr.length;i++) {
+
+     if (typeof arr[i] === "number") {
+         number.push(arr[i]);
+         // console.log(number);
+     }
+     else if (typeof arr[i] === "string") {
+         string.push(arr[i]);
+         // console.log(string);
+     }
+     else {
+         // if (typeof arr[i][i]==="number") {
+         let arrIntoArr = arr[i];
+         // arrIntoArr=arrIntoArr[2];
+         // let deepArrArr= arrIntoArr[0];
+
+         for (k = 0; k < arrIntoArr.length; k++) {
+
+             if (typeof arrIntoArr[k] === "number") {
+                 number.push(arrIntoArr[k]);
+             }
+             // console.log(arrIntoArr[k]);
+             else {
+                 string.push(arrIntoArr[k]);
+                 // console.log(arrIntoArr[k]);
+             }
+             // console.log(arrIntoArr.length);
+
+         }
+
+     }
+ }
+    console.log(number);
+    console.log(string);
+
